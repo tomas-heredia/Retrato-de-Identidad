b06ca615452ac374901248fa2936de7d2b515b10
tomas-heredia <69855938+tomas-heredia@users.noreply.github.com>
2025-11-21 17:09:46 -0300

    funcion de huellas

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,10 +4,12 @@ var recolectados := 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	recolectables.text = "0"
+	recolectables.text = "0/5"
 	Mensajero.connect("recolectable", aumentar_recolectable)
 	
 
 func aumentar_recolectable():
 	recolectados += 1
-	recolectables.text = str(recolectados)
+	recolectables.text = str(recolectados) + "/5"
+	if recolectados >= 5:
+		Global.puede_continuar = true
