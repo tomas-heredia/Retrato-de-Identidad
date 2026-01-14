dc835067b798d66000516005356ddd284e7f46ca
tomas-heredia <69855938+tomas-heredia@users.noreply.github.com>
2026-01-14 11:16:13 -0800

    correcciones transicion

--- scripts/transiciones.gd
+++ scripts/transiciones.gd
@@ -4,7 +4,7 @@ signal iniciar
 @export var inicio: bool
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ManejoNiveles.connect("fadeOut",FadeOut)
+	Mensajero.connect("fadeOut",FadeOut)
 	
 	$AnimationPlayer.play("FadeIn")
 	
