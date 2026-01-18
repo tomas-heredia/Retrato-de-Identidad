42655934be8a81a609f22774b2af084ab44c6ad0
tomas-heredia <69855938+tomas-heredia@users.noreply.github.com>
2026-01-18 10:56:16 -0800

    plataforma invisible
    se creo la plataforma y se la puede asociar a recuerdos

--- scripts/recolectable.gd
+++ scripts/recolectable.gd
@@ -1,5 +1,6 @@
 extends Area3D
 
+@export var RigidBody_relacionados: Array[RigidBody3D] = []
 @onready var animation_player = $cuerpo/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
@@ -10,5 +11,10 @@ func _ready():
 
 func _on_body_entered(objeto):
 	if objeto.is_in_group("Player"):
+
+		for elemento in RigidBody_relacionados:
+			if elemento.is_in_group("plataforma_invisible"):
+				elemento.activar()
+
 		Mensajero.recolectable.emit()
 		self.queue_free()
