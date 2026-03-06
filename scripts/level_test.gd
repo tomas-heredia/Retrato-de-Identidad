dbc5461718616eae9514554091a6f27a9ea3991f
tomas-heredia <69855938+tomas-heredia@users.noreply.github.com>
2026-03-06 15:44:13 -0800

    menuses

--- scripts/level_test.gd
+++ scripts/level_test.gd
@@ -4,4 +4,4 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.interactuando = false
-	
+	Global.nivel_actual = "level_test"
--- scripts/title_scene.gd
+++ scripts/title_scene.gd
@@ -8,22 +8,27 @@ var MUSIC_OFF := preload("res://Assets/UI/Title_scene/MUSIC-OFF.PNG.png")
 @onready var sfx = $SFX
 @onready var music = $Music
 @onready var salir = $Salir
+@onready var confirmacion: Control = $Confirmacion
+@onready var jugar: Button = $Jugar
+@onready var controles_display: Control = $Controles_display
 
 
 
 func _ready() -> void:
 	Guardado.load_game()
-	
+	confirmacion.hide()
+	controles_display.hide()
+	if !Guardado.existe_guardado():
+		jugar.disabled =true
 
 
 func _on_jugar_pressed():
-	Guardado.sobreescribir()
+	
 	ManejoNiveles.cambiar("level_hub_world")
 
 
-func _on_opciones_pressed():
-	pass
-
+func _on_controles_pressed() -> void:
+	controles_display.show()
 
 func _on_creditos_pressed():
 	pass # Replace with function body.
@@ -45,3 +50,31 @@ func _on_music_pressed():
 
 func _on_salir_pressed():
 	get_tree().quit()
+
+
+func _on_nuevo_juego_pressed() -> void:
+	if Guardado.existe_guardado():
+		confirmacion.show()
+		for boton in [$Jugar, $Nuevo_juego, $Opciones, $Creditos, $SFX, $Music, $Salir]:
+			boton.disabled = true
+
+	else:
+		Guardado.sobreescribir()
+		ManejoNiveles.cambiar("level_hub_world")
+
+
+func _on_si_pressed() -> void:
+	
+	ManejoNiveles.cambiar("level_hub_world")
+
+
+func _on_no_pressed() -> void:
+	confirmacion.hide()
+	for boton in [$Jugar, $Nuevo_juego, $Opciones, $Creditos, $SFX, $Music, $Salir]:
+			boton.disabled = false
+	if !Guardado.existe_guardado():
+		jugar.disabled =true
+
+
+func _on_volver_pressed() -> void:
+	controles_display.hide()
