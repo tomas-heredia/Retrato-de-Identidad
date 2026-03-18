fab5c02fccd83e1d42def9f2069acc3e1db35aba
fcapovilla2502 <148599893+fcapovilla2502@users.noreply.github.com>
2026-03-17 21:42:37 -0300

    llamita

--- scripts/recolectable.gd
+++ scripts/recolectable.gd
@@ -4,8 +4,6 @@ extends Area3D
 @onready var animation_player = $cuerpo/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	animation_player.play("flotar")
 	
 
 
